f8e3403ea969ce4369ce8d770c6557dc29957002
Srushtikaulwar <160225455+Srushtikaulwar@users.noreply.github.com>
2026-06-24 17:27:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let student={
+	name: "srushti",
+	lastanme: "kaulwar",
+	age: 21
+}
+function getkey(obj){
+	return Object.keys(obj)
+	
+	
+}console.log(getkey(student))
